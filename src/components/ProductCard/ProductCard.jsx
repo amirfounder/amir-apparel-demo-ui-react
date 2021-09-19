59e1f383967716d166532034ac4884b66d65a119
Amir Sharapov <amir@amirfounder.com>
2021-09-19 00:57:42 -0500

    made header sticky and styled the cards

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import constants from "../../utils/constants";
+import { Button } from "../Button";
+import { Heading } from "../Heading";
 import styles from './ProductCard.module.scss'
 
 export const ProductCard = (props) => {
@@ -8,9 +11,41 @@ export const ProductCard = (props) => {
 
   return (
     <div className={styles.main}>
-      {product.name}
-      {product.price}
-      {product.description}
+      <div
+        className={styles.cardHeader}
+        style={{
+          backgroundImage: `url(${constants.PLACEHOLDER_IMAGE_URL})`,
+          backgroundSize: 'cover',
+          backgroundColor: '#ddd',
+          backgroundPosition: 'center'
+        }}
+      />
+      <div className={styles.cardBody}>
+        <div className={styles.heading}>
+          <Heading
+            level={3}
+          >
+            {product.name}
+          </Heading>
+        </div>
+        <p className={styles.paragraph}>
+          {product.description}
+        </p>
+        <div className={styles.price}>
+          <span className={styles.priceLabel}>Your investment: </span>
+          <span className={styles.priceValue}>${product.price.toFixed(2)}</span>
+        </div>
+        <Button
+          style={{
+            backgroundColor: 'black',
+            padding: '6px 20px',
+            fontSize: '14px',
+            fontWeight: 300
+          }}
+        >
+          Add to Cart
+        </Button>
+      </div>
     </div>
   )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 export default Object.freeze({
   BASE_API_ENDPOINT: "http://localhost:8085",
-  PRODUCTS_ENDPOINT: "/products"
+  PRODUCTS_ENDPOINT: "/products",
+  PLACEHOLDER_IMAGE_URL: 'https://www.gaithersburgdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png'
 })
