8e9b2a31d508c2afe80261c075275abac614c9f6
Amir Sharapov <amir@amirfounder.com>
2021-09-21 14:44:38 -0500

    updated header design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "../Button";
 import styles from './Header.module.scss'
 import { useHistory } from "react-router-dom";
+import { BiCartAlt, BiShoppingBag } from 'react-icons/bi'
 
 export const Header = () => {
+  const history = useHistory()
 
-  const history = useHistory();
-
-  const handleLoginClick = () => {
-    history.push('/login')
-  }
+  const handleCartIconClick = () => history.push('/cart')
 
   return (
     <div className={styles.main}>
-      <div className={styles.column}>
-        <Link to="/" className={styles.navLink}>Amir Apparel Demo</Link>
-      </div>
-      <div className={styles.column}>
-        <div className={styles.menu}>
-          <Link to="/about" className={styles.navLink}>About</Link>
-          <Link to="/shop" className={styles.navLink}>Shop</Link>
-          <Link to="/contact" className={styles.navLink}>Contact</Link>
+      <div className={styles.top}>
+        <div className={styles.column}>
+          <div>
+            Contact Support
+          </div>
+          <div>
+            FREE Shipping on Orders of $100+
+          </div>
+        </div>
+        <div className={styles.column}>
+          <Link to="/register" className={styles.navLink}>Register</Link>
+          <Link to="/login" className={styles.navLink}>Login</Link>
+          <Link to="/contact" className={styles.navLink}>Feedback</Link>
         </div>
       </div>
-      <div className={styles.column}>
-        <div className={styles.loginRegisterLinks}>
-          <Link to="/register" className={styles.navLink}>Register</Link>
-          <Button onClick={handleLoginClick}>
-            Login
-          </Button>
+      <div className={styles.bottom}>
+        <div className={styles.column}>
+          <Link to="/" className={styles.navLink}>
+            AA
+          </Link>
+        </div>
+        <div className={styles.column}>
+          <div className={styles.menu}>
+            <Link to="/shop" className={styles.navLink}>New Releases</Link>
+            <Link to="/shop" className={styles.navLink}>Men</Link>
+            <Link to="/shop" className={styles.navLink}>Women</Link>
+            <Link to="/shop" className={styles.navLink}>Kids</Link>
+            <Link to="/shop" className={styles.navLink}>Sale</Link>
+          </div>
+        </div>
+        <div className={styles.column}>
+          <BiShoppingBag
+            className={styles.icon}
+            size='30px'
+          />
+          <BiCartAlt
+            onClick={handleCartIconClick}
+            className={styles.icon}
+            size='30px'
+          />
         </div>
       </div>
     </div>
